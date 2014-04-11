5c6bfea97e90eec5d311a135dafc9264fd1dae14
Vitor Bertazoli <vitor@bertazoli.com>
2014-04-10 22:53:47 -0400

    add some validation to sign up

--- src/main/java/com/bertazoli/charity/client/application/signup/SignupView.java
+++ src/main/java/com/bertazoli/charity/client/application/signup/SignupView.java
@@ -1,6 +1,7 @@
 package com.bertazoli.charity.client.application.signup;
 
 import java.sql.Timestamp;
+import java.util.Date;
 
 import javax.inject.Inject;
 
@@ -12,12 +13,18 @@ import com.gwtplatform.mvp.client.ViewImpl;
 import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.event.SelectEvent.HasSelectHandlers;
 import com.sencha.gxt.widget.core.client.form.DateField;
+import com.sencha.gxt.widget.core.client.form.FormPanel;
 import com.sencha.gxt.widget.core.client.form.PasswordField;
 import com.sencha.gxt.widget.core.client.form.TextField;
+import com.sencha.gxt.widget.core.client.form.validator.MaxDateValidator;
+import com.sencha.gxt.widget.core.client.form.validator.MaxLengthValidator;
+import com.sencha.gxt.widget.core.client.form.validator.MinLengthValidator;
+import com.sencha.gxt.widget.core.client.form.validator.RegExValidator;
 
 public class SignupView extends ViewImpl implements SignupPresenter.MyView {
     interface Binder extends UiBinder<Widget, SignupView> {}
 
+    @UiField FormPanel mainPanel;
     @UiField TextField username;
     @UiField TextField firstname;
     @UiField TextField lastname;
@@ -30,6 +37,27 @@ public class SignupView extends ViewImpl implements SignupPresenter.MyView {
     @Inject
     SignupView(Binder uiBinder) {
         initWidget(uiBinder.createAndBindUi(this));
+        username.setAllowBlank(false);
+        username.addValidator(new MinLengthValidator(5));
+        username.addValidator(new MaxLengthValidator(40));
+        firstname.setAllowBlank(false);
+        firstname.addValidator(new MinLengthValidator(3));
+        firstname.addValidator(new MaxLengthValidator(40));
+        lastname.setAllowBlank(false);
+        lastname.addValidator(new MinLengthValidator(3));
+        lastname.addValidator(new MaxLengthValidator(40));
+        password.setAllowBlank(false);
+        password.addValidator(new MinLengthValidator(8));
+        password.addValidator(new MaxLengthValidator(30));
+        confirmPassword.setAllowBlank(false);
+        confirmPassword.addValidator(new MinLengthValidator(8));
+        confirmPassword.addValidator(new MaxLengthValidator(30));
+        email.setAllowBlank(false);
+        email.addValidator(new MinLengthValidator(5));
+        email.addValidator(new MaxLengthValidator(40));
+        email.addValidator(new RegExValidator("^[a-z0-9]+@[a-z0-9].[a-z0-9]+", "aa"));
+        dob.setAllowBlank(false);
+        dob.addValidator(new MaxDateValidator(new Date()));
     }
 
     @Override
@@ -39,8 +67,7 @@ public class SignupView extends ViewImpl implements SignupPresenter.MyView {
 
     @Override
     public boolean validate() {
-        // TODO validate
-        return true;
+        return mainPanel.isValid();
     }
 
     @Override
